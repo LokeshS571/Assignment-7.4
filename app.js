fdea7507dddea03495cbab0e3cc0cfb2c3061ee4
LokeshS571 <lokeshv542@gmail.com>
2017-03-26 15:23:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+// outer function
 function outerfunction(myfunction)
 {
 	myfunction();
 }
-  
+// inner functon  
 function innerfunction()
 {
 	console.log("Hello World From Inner Function");
 }
-
+//a call to innrefunction through outerfunction
 outerfunction(innerfunction);
 
